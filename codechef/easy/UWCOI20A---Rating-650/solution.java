198bfbb278b1d744746e05715d1b58dda790e4d5
Nagabhushana N <168909081+Nagu-developer-001@users.noreply.github.com>
2026-09-02 16:59:33 +0530

    ✅ UWCOI20A - Rating 650 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/easy/UWCOI20A---Rating-650/solution.java
+++ codechef/easy/UWCOI20A---Rating-650/solution.java
@@ -6,26 +6,23 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	    Scanner sc = new Scanner(System.in);
 		// your code goes here
-		int maxN = 0;
-		int t = sc.nextInt();
-		while(t-- > 0){
-		    int n = sc.nextInt();
-		    int[] arr = new int[n];
-		    for(int i=0;i<n;i++){
-		        arr[i] = sc.nextInt();
-		    }    
-		    maxN= arr[0];
-		    for(int i=1;i<n;i++){
-		        if(arr[i]>maxN){
-		            maxN = arr[i];
-		        }
-		    }
-		
-		    System.out.println(maxN);
-		}
-		
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int N = sc.nextInt();
+            int arr[] = new int[N];
+            for(int i=0;i<N;i++){
+                arr[i] = sc.nextInt();
+            }
+            int max = arr[0];
+            for(int i=1;i<N;i++){
+                if(arr[i]>max){
+                    max = arr[i];
+                }
+            }
+            System.out.println(max);
+        }
         
 	}
 }
