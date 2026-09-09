50cda12eb28897bf51ca80fe5308ef02bc4b097b
Nagabhushana N <168909081+Nagu-developer-001@users.noreply.github.com>
2026-09-09 21:21:51 +0530

    ✅ MAXCONSECU | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/MAXCONSECU/solution.java
+++ codechef/medium/MAXCONSECU/solution.java
@@ -1,10 +1,14 @@
     public static int findMaxConsecutiveOnes(int[] nums) {
        //write code here...
-       int cnt = 0;
-       for(int i=0;i<nums.length-1;i++){
-           if(nums[i]==1&&nums[i+1]==1){
-               cnt+=2;
+       int streak = 0;
+       int maxStreak = 0;
+       for(int num : nums){
+           if(num==1){
+               streak += 1;
+               maxStreak = Math.max(streak,maxStreak);
+           }else{
+               streak = 0;
            }
        }
-       return cnt;
+       return maxStreak;
     }
